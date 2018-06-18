0fabefaee9baf47706819208219643ac0866ffd7
Joshua Suskin <jsuskin85@gmail.com>
2018-06-18 04:33:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,16 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 var newElement;
 
+<<<<<<< HEAD
 function addElementToBeginningOfArray(chocolateBars, newElement) {
+=======
+function addElementToBeginningOfArray() {
+>>>>>>> a6387ebb7d31ec49859a581360278c4bd454b2cb
   chocolateBars = [newElement, ...chocolateBars];
   return chocolateBars;
 }
 
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(chocolateBars, newElement) {
   chocolateBars.unshift(newElement);
   return chocolateBars;
@@ -42,5 +47,9 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 
 function removeElementFromEndOfArray(chocolateBars) {
   chocolateBars = chocolateBars.slice(0, chocolateBars.length - 1);
+=======
+function destructivelyAddElementToBeginningOfArray() {
+  chocolateBars = chocolateBars.unshift(newElement);
+>>>>>>> a6387ebb7d31ec49859a581360278c4bd454b2cb
   return chocolateBars;
 }
